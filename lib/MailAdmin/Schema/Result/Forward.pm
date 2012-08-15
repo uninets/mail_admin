de2e09f89a74e77b9f850fd50f8262c09fc409af
Mugen Kenichi <mugen.kenichi@uninets.eu>
2012-08-15 13:58:24 +0200

    fixed autoincrementing id on forwards table new default password is 'password'

--- lib/MailAdmin/Schema/Result/Forward.pm
+++ lib/MailAdmin/Schema/Result/Forward.pm
@@ -1,4 +1,5 @@
 use utf8;
+
 package MailAdmin::Schema::Result::Forward;
 
 # Created by DBIx::Class::Schema::Loader
@@ -38,7 +39,9 @@ __PACKAGE__->table("forwards");
 =head2 id
 
   data_type: 'integer'
+  is_auto_increment: 1
   is_nullable: 0
+  sequence: 'forwards_id_seq'
 
 =head2 created_at
 
@@ -69,26 +72,31 @@ __PACKAGE__->table("forwards");
 =cut
 
 __PACKAGE__->add_columns(
-  "id",
-  { data_type => "integer", is_nullable => 0 },
-  "created_at",
-  {
-    data_type     => "timestamp",
-    default_value => \"current_timestamp",
-    is_nullable   => 0,
-    original      => { default_value => \"now()" },
-  },
-  "updated_at",
-  {
-    data_type     => "timestamp",
-    default_value => \"current_timestamp",
-    is_nullable   => 0,
-    original      => { default_value => \"now()" },
-  },
-  "address",
-  { data_type => "varchar", is_nullable => 0, size => 255 },
-  "email_id",
-  { data_type => "integer", is_foreign_key => 1, is_nullable => 0 },
+    "id",
+    {
+        data_type         => "integer",
+        is_auto_increment => 1,
+        is_nullable       => 0,
+        sequence          => "forwards_id_seq"
+    },
+    "created_at",
+    {
+        data_type     => "timestamp",
+        default_value => \"current_timestamp",
+        is_nullable   => 0,
+        original      => { default_value => \"now()" },
+    },
+    "updated_at",
+    {
+        data_type     => "timestamp",
+        default_value => \"current_timestamp",
+        is_nullable   => 0,
+        original      => { default_value => \"now()" },
+    },
+    "address",
+    { data_type => "varchar", is_nullable => 0, size => 255 },
+    "email_id",
+    { data_type => "integer", is_foreign_key => 1, is_nullable => 0 },
 );
 
 =head1 PRIMARY KEY
@@ -113,17 +121,10 @@ Related object: L<MailAdmin::Schema::Result::Email>
 
 =cut
 
-__PACKAGE__->belongs_to(
-  "email",
-  "MailAdmin::Schema::Result::Email",
-  { id => "email_id" },
-  { is_deferrable => 1, on_delete => "CASCADE", on_update => "CASCADE" },
-);
-
+__PACKAGE__->belongs_to( "email", "MailAdmin::Schema::Result::Email", { id => "email_id" }, { is_deferrable => 1, on_delete => "CASCADE", on_update => "CASCADE" }, );
 
 # Created by DBIx::Class::Schema::Loader v0.07024 @ 2012-06-07 14:53:09
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:FBA11ih0oYgE/qGBBSI03Q
-
 
 # You can replace this text with custom code or comments, and it will be preserved on regeneration
 1;
--- script/deploydb.pl
+++ script/deploydb.pl
@@ -9,6 +9,7 @@ use MailAdmin::Schema;
 my $schema = MailAdmin::Schema->connect('dbi:Pg:dbname=mailadmin;host=localhost;user=mailadmin;password=mailadmin;');
 $schema->deploy;
 
+# default admin:password
 $schema->resultset('Role')->create({ name => 'admin', id => 1 });
-$schema->resultset('User')->create({ login => 'admin', email => 'admin@example.com', role_id => 1, password => 'CY9rzUYh03PK3k6DJie09g'});
+$schema->resultset('User')->create({ login => 'admin', email => 'admin@example.com', role_id => 1, password => '5f4dcc3b5aa765d61d8327deb882cf99'});
 
